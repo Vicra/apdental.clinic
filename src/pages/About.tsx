c9640638e22102b1e139b4a73939cd8d7ebbd15f
Vicra <honvramirez@gmail.com>
2023-04-24 22:05:00 -0600

    remove unused comp

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,7 @@
-import ParallaxImage from "../components/ParallaxImage";
-
 const About: React.FC = () => {
   return (
     <>
       <h1>About</h1>
-      <ParallaxImage
-        buttonText="Button"
-        imageUrl="https://merakidentalstudio.com/wp-content/uploads/2021/09/Meraki-dental-studio-best-dental-clinic-in-india.jpg"
-      />
     </>
   );
 };
