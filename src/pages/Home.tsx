b826bbace1d6720038b9cd22d5e41a5723099364
Vicra <honvramirez@gmail.com>
2023-05-02 22:39:16 -0600

    BeforeNAfter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,17 @@ import { Layout } from "antd";
 import Banner from "../common/Banner";
 import LocationMap from "../common/LocationMap/LocationMap";
 import OurServices from "../components/OurServices";
-// import YoutubeEmbed from "../components/YoutubeEmbed";
+import ReactBeforeSliderComponent from "react-before-after-slider-component";
+import "react-before-after-slider-component/dist/build.css";
 
 const Home: React.FC = () => {
+  const FIRST_IMAGE = {
+    imageUrl: "/beforeNafter/blanqueamiento2.png",
+  };
+  const SECOND_IMAGE = {
+    imageUrl: "/beforeNafter/blanqueamiento1.png",
+  };
+
   return (
     <>
       <Layout style={{ margin: "2rem", backgroundColor: "white" }}>
@@ -12,6 +20,20 @@ const Home: React.FC = () => {
         <br />
         <OurServices />
         <LocationMap latitude={15.5024} longitude={-88.040876} />
+
+        <Layout
+          style={{
+            margin: "2rem",
+            backgroundColor: "white",
+            maxWidth: "500px",
+          }}
+        >
+          {/* <ReactBeforeSliderComponent
+            firstImage={FIRST_IMAGE}
+            secondImage={SECOND_IMAGE}
+            delimiterColor="black"
+          /> */}
+        </Layout>
       </Layout>
       {/* <YoutubeEmbed /> */}
       {/* <Slider /> */}
